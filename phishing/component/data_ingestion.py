653dc9cffc4d1cbbdd8ae1bfc221581554f47e5a
hiteshwadhwani <developerhitesh1403@gmail.com>
2023-01-15 15:18:48 +0530

    updated

--- phishing/component/data_ingestion.py
+++ phishing/component/data_ingestion.py
@@ -1,5 +1,15 @@
+import pandas as pd
+import numpy as np
+from phishing.entity import config_entity, artifact_entity
+from phishing.logger import logging
+from phishing.exception import PhishingException
+from sklearn.model_selection import train_test_split
+
+
 class Data_ingestion:
-    def __init__(self):
-        pass
+    def __init__(self, data_ingestion_config:config_entity.Data_ingestion_config):
+        logging.info(f"{2 * '<<'} DATA INGESTION {2 * '>>'}")
+        self.data_ingestion_config = data_ingestion_config
+
     def intiate_data_ingestion():
         pass
---
+++ phishing/entity/config_entity.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+class Data_ingestion_config:
+    pass
+
+class Data_transformation_config:
+    pass
+
+class Data_validation_config:
+    pass
+
+class Model_builder_config:
+    pass
+
+class Model_evaluation_config:
+    pass
+
+class Model_pusher_config:
+    pass
+
